bed2995548660dbc84c49f5a3bc551d3e8e7445a
kulaginvl <vladkulagin0801@gmail.com>
2021-05-31 11:49:51 +0300

    Styled imagesetion

--- src/components/ImageSection.js
+++ src/components/ImageSection.js
@@ -1,35 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
+import sectionImg from '../assets/img/img-section.jpg';
 
 const ImageSection = () => {
   return (
     <ImageSectionStyled>
       <div className="left-content">
-        <img src={null} alt="img" />
+        <img src={sectionImg} alt="img" />
       </div>
       <div className="right-content">
-        <div className="sub-title">
-          <h4>
-            I am <span>Vlad</span>
-          </h4>
-          <p className="pargraph">
-            Quis et cillum enim reprehenderit veniam incididunt pariatur qui laborum ex voluptate
-            excepteur. Cillum eu incididunt eu cupidatat eiusmod sunt aliquip et exercitation. Lorem
-            proident quis sit aute dolor do.
-          </p>
-          <div className="a-inf">
-            <div className="inf-title">
-              <p>Full Name</p>
-              <p>Age</p>
-              <p>Languages</p>
-              <p>Location</p>
-            </div>
-            <div className="inf">
-              <p>: Vladislav</p>
-              <p>: 22</p>
-              <p>: Rus, Eng</p>
-              <p>: Russia</p>
-            </div>
+        <h4>
+          I am <span>Vlad</span>
+        </h4>
+        <p className="pargraph">
+          Quis et cillum enim reprehenderit veniam incididunt pariatur qui laborum ex voluptate
+          excepteur. Cillum eu incididunt eu cupidatat eiusmod sunt aliquip et exercitation. Lorem
+          proident quis sit aute dolor do.
+        </p>
+        <div className="a-inf">
+          <div className="inf-title">
+            <p>Full Name</p>
+            <p>Age</p>
+            <p>Languages</p>
+            <p>Location</p>
+          </div>
+          <div className="inf">
+            <p>: Vladislav</p>
+            <p>: 22</p>
+            <p>: Rus, Eng</p>
+            <p>: Russia</p>
           </div>
         </div>
       </div>
@@ -37,6 +36,43 @@ const ImageSection = () => {
   );
 };
 
-const ImageSectionStyled = styled.div``;
+const ImageSectionStyled = styled.div`
+  margin-top: 5rem;
+  display: flex;
+
+  .left-content {
+    width: 150%;
+    img {
+      width: 90%;
+    }
+  }
+  .right-content {
+    h4 {
+      font-size: 2rem;
+      color: var(--white-color);
+      span {
+        font-size: 2rem;
+      }
+    }
+    .pargraph {
+      padding: 1rem 0;
+    }
+    .a-inf {
+      display: flex;
+      .inf-title {
+        padding-right: 2rem;
+        p {
+          font-weight: 600;
+        }
+      }
+      .inf-title,
+      .inf {
+        p {
+          padding: 0.3rem 0;
+        }
+      }
+    }
+  }
+`;
 
 export default ImageSection;
